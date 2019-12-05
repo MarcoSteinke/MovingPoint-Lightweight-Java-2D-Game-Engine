e8df0d8bb5e8842db41a3bbd49dc09e86f40eb82
maste150hhu <mastremolten2@gmail.com>
2019-12-05 22:14:09 +0100

    add cleanup.sh for WINDOWS

---
+++ cleanup.bat
@@ -0,0 +1,8 @@
+# build sh written for the MovingPoint project
+# change directory to the projects src:
+cd src
+
+# remove unwanted files and prepare the git proccess:
+echo MovingPoint: Clean Project Files
+del *-BestOfCode.java
+del *.class
