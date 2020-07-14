2946ed232d36474b1d5f6c8fdbc4683d64b84bad
maste150hhu <mastremolten2@gmail.com>
2020-07-14 17:19:18 +0200

    update KeyboardConfiguration

--- src/main/java/net/bestofcode/MovingPoint/event/keyboardEvent/configuration/KeyboardConfiguration.java
+++ src/main/java/net/bestofcode/MovingPoint/event/keyboardEvent/configuration/KeyboardConfiguration.java
@@ -1,4 +1,15 @@
 package net.bestofcode.MovingPoint.event.keyboardEvent.configuration;
 
+import java.util.HashMap;
+
 public class KeyboardConfiguration implements Configuration {
+
+    private final HashMap<DefaultKey, Character> personKeyConfiguration;
+
+    private KeyboardConfiguration(DefaultKey... keys) {
+        this.personKeyConfiguration = new HashMap<>();
+        for(DefaultKey key : keys) {
+            personKeyConfiguration.put(key, ' ');
+        }
+    }
 }
