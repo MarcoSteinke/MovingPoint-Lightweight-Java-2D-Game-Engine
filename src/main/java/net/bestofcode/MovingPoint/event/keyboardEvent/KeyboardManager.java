2b13c3f690252dfea920d9a51e0a59dbd24d05fb
maste150hhu <mastremolten2@gmail.com>
2020-07-14 16:42:02 +0200

    split project into more components

---
+++ src/main/java/net/bestofcode/MovingPoint/event/keyboardEvent/KeyboardManager.java
@@ -0,0 +1,15 @@
+package net.bestofcode.MovingPoint.event.keyboardEvent;
+
+public class KeyboardManager {
+
+    public int keyUp = 87;
+    public int keyLeft = 65;
+    public int keyRight = 68;
+    public int keyDown = 83;
+    public boolean keyUpPressed = false;
+    public boolean keyLeftPressed = false;
+    public boolean keyRightPressed = false;
+    public boolean keyDownPressed = false;
+
+
+}
