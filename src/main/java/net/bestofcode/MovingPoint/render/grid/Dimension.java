202fbf2c95260fec6acbc7785bc2243474b26174
maste150hhu <mastremolten2@gmail.com>
2020-07-16 00:57:26 +0200

    start implementation of grid

---
+++ src/main/java/net/bestofcode/MovingPoint/render/grid/Dimension.java
@@ -0,0 +1,14 @@
+package net.bestofcode.MovingPoint.render.grid;
+
+class Dimension {
+
+    int value;
+
+    Dimension(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/main/java/net/bestofcode/MovingPoint/render/grid/Grid.java
@@ -0,0 +1,10 @@
+package net.bestofcode.MovingPoint.render.grid;
+
+public class Grid {
+
+    private final Dimension dimension;
+
+    public Grid(Dimension dimension) {
+        this.dimension = dimension;
+    }
+}
